983aaa667fa9f69d3ec1365b4645bc1a63c6c772
Courtney Harris <cjharris1985@gmail.com>
2023-07-12 17:31:43 -0500

    started working on site

---
+++ src/components/hero.js
@@ -0,0 +1,17 @@
+export default function Hero() {
+   return (
+      <section className='text-gray-600 '>
+         <div className='container px-5 py-24 mx-auto '>
+            <div className='flex flex-col items-start mx-auto lg:w-2/3 sm:flex-row sm:items-center'>
+               <h1 className='flex-grow text-2xl font-medium text-gray-900 sm:pr-16 title-font'>
+                  Slow-carb next level shoindxgoitch ethical
+                  authentic, scenester sriracha forage
+               </h1>
+               <button className='flex-shrink-0 px-8 py-2 mt-10 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600 sm:mt-0'>
+                  Button
+               </button>
+            </div>
+         </div>
+      </section>
+   );
+}
